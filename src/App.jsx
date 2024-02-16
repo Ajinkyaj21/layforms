c32e97c3d6307c081abe74aba76de8b3f4fffd49
Ajinkyaj21 <ajinkya.jagadale@flairminds.com>
2024-02-16 17:07:37 +0530

    added drop changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const reorder = (list, startIndex, endIndex) => {
 const App=() =>{
 
   const [items, setItems] = useState([]);
+  const [formItems,setFormItems]=useState([])
 
   useEffect(() => {
     setItems(demoData);
@@ -24,21 +25,43 @@ const App=() =>{
     if (!result.destination) {
       return;
     }
+    
+    // if (result.destination.droppableId === "droppable") {
+   
+    // }
+    else if (result.destination.droppableId === "form-droppable") {
+      
+      const item = items.filter((item) => item.id == result.draggableId) 
 
-    const reorderedItems = reorder(
-      items,
-      result.source.index,
-      result.destination.index
-    );
+      const valExists=formItems.some(value=> value== item[0])
+      console.log(valExists,item[0],items,"lulz");
+      
+      const reorderedItems = reorder(
+        items,
+        result.source.index,
+        result.destination.index
+      );
+  
+      console.log({ reorderedItems });
+      // setFormItems(reorderedItems);
 
-    console.log({ reorderedItems });
-    setItems(reorderedItems);
+      if(!valExists){
+        setFormItems((prev)=>[...prev,item[0]]);
+      }
+    
+    }
   };
+
+  useEffect(()=>{
+    console.log(formItems,"lulzo");
+  },[formItems])
+ 
+
   return (
     <>
-      <h2 className='text-center mt-5 fw-bold'>FORM</h2> 
+      {/* <h2 className='text-center mt-5 fw-bold'>FORM</h2>  */}
      <div className={styles.main_content}>
-
+     <div className='d-flex'>
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided, snapshot) => (
@@ -68,7 +91,35 @@ const App=() =>{
             </div>
           )}
         </Droppable>
+
+        <div className='mx-5'>
+          <h1>Form</h1>
+          <form>
+          <Droppable droppableId="form-droppable">
+  {(provided, snapshot) => (
+    <form ref={provided.innerRef} {...provided.droppableProps}>
+      {formItems.map((item,index) => (
+        <Draggable key={item.id} draggableId={item.id} index={index}>
+        {(provided, snapshot) => (
+         <div
+         ref={provided.innerRef}
+         {...provided.draggableProps}
+         {...provided.dragHandleProps}
+         className={styles.inputContainer}
+                             >
+        <input  className={styles.input} key={item.id} placeholder={item.content}></input>
+        </div>
+            )}
+            </Draggable>
+      ))}
+      {provided.placeholder}
+    </form>
+  )}
+</Droppable>
+          </form>
+        </div>
       </DragDropContext>
+      </div>
     </div>
   </>
   )
